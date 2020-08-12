8b4a62964cc341c281a36cbedba69500d7865f01
Leticia Cardozo <leticiacardozo_1@hotmail.com>
2020-08-12 09:57:46 -0300

    firsttt

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+{{-- Título da página --}}
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Painel de Controle</h1>
+@endsection
+
+{{-- Conteúdo da página --}}
+@section('content')
+    oeioeieoei
+@endsection
+
+{{-- Adicionar css na página --}}
+@section('css')
+    {{-- <link rel="stylesheet" href="...." />     --}}
+@endsection
+
+{{-- Adicionar JS na página --}}
+@section('js')
+    {{-- <script>alert('Rodando')</script> --}}
+@endsection
